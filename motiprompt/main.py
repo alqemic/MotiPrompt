daca7fdc87442610157be00ec1fc971842530e69
alqemic <alqemic@C-CND2320SJ4>
2024-03-05 23:24:28 +0100

    Add random number generation while pressing the button

--- motiprompt/main.py
+++ motiprompt/main.py
@@ -1,14 +1,25 @@
 import kivy
-kivy.require('2.3.0')
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+
+import random
+
+kivy.require('2.3.0')
+
+
+class MyRoot(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(MyRoot, self).__init__(**kwargs)
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
 
 
 class MotiPrompt(App):
 
     def build(self):
-        return BoxLayout()
+        return MyRoot()
 
 
 if __name__ == '__main__':
