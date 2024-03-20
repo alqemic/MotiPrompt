46b3d39463c6d8292243ae6e3c52d72323edf5c9
alqemic <15896760+alqemic@users.noreply.github.com>
2024-03-20 23:10:05 +0100

    Add dropdown menu to ShowQuotes

--- motiprompt/screens.py
+++ motiprompt/screens.py
@@ -1,4 +1,5 @@
 import json
+from kivy.logger import Logger
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -6,6 +7,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
 from kivymd.uix.button import MDRoundFlatIconButton
+from kivymd.uix.menu import MDDropdownMenu
 
 import random
 
@@ -66,11 +68,15 @@ class ShowQuotes(Screen):
         super(ShowQuotes, self).__init__(**kwargs)
         self.bind(size=self._update_text_size)
 
+        self.quote_sets = ["default.json", "set1.json", "set2.json"]
+        self.current_set = "default.json"
+
         self.scroll_view = ScrollView()
         self.scroll_view.do_scroll_y = True
         self.scroll_view.do_scroll_x = True
 
         self.layout = BoxLayout(orientation='vertical')
+
         self.refresh_quotes()
         self.scroll_view.add_widget(self.layout)
         self.add_widget(self.scroll_view)
@@ -85,6 +91,21 @@ class ShowQuotes(Screen):
 
     def refresh_quotes(self):
         self.layout.clear_widgets()
+        Logger.info("ShowQuotes: Creating dropdown menu")
+        self.dropdown_menu = MDDropdownMenu(
+            pos_hint={'center_x': .5, 'center_y': .5},
+            width_mult=4,
+        )
+        Logger.info("ShowQuotes: Creating dropdown button")
+        self.dropdown_button = MDRoundFlatIconButton(
+            text=self.current_set,
+            icon='menu',
+            pos_hint={'center_x': .5, 'center_y': .9},
+            on_release=self.dropdown_menu.open
+        )
+        self.build_dropdown_menu()
+
+        self.layout.add_widget(self.dropdown_button)
 
         with open('motiprompt/quotes/default.json', 'r') as file:
             quotes = json.load(file)
@@ -105,3 +126,19 @@ class ShowQuotes(Screen):
 
     def go_to_main(self, instance):
         self.manager.current = 'main'
+
+    def build_dropdown_menu(self):
+        for quote_set in self.quote_sets:
+            self.dropdown_menu.items.append(
+                {
+                    "viewclass": "OneLineListItem",
+                    "text": quote_set,
+                    "on_release": lambda x=quote_set: self.select_quote_set(x)
+                }
+            )
+
+    def select_quote_set(self, quote_set):
+        self.current_set = quote_set
+        self.dropdown_button.text = f"Select Set: {quote_set}"
+        self.refresh_quotes()
+        self.dropdown_menu.dismiss()
