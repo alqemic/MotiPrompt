f971eb1aa0903f53e3cd8275daff3a96b9c4cb88
alqemic <15896760+alqemic@users.noreply.github.com>
2024-04-16 23:24:13 +0200

    Add unit tests for screens

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 import unittest
 
+from kivy.uix.screenmanager import Screen
+
 from motiprompt.main import MotiPrompt
 
 
@@ -13,3 +15,16 @@ class TestMotiPrompt(unittest.TestCase):
     def test_guiExists(self):
         gui = self.app.build()
         assert gui is not None
+
+    def test_widgetsExists(self):
+        gui = self.app.build()
+        widgets = gui.children
+        self.assertIsNotNone(widgets)
+        self.assertEqual(len(widgets), 1)
+
+    def test_screenExists(self):
+        gui = self.app.build()
+        widgets = gui.children
+        screen = widgets[0]
+        self.assertIsNotNone(screen)
+        self.assertIsInstance(screen, Screen)
