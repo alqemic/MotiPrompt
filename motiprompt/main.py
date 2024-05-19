59d6a5e225677e2a0c2ccae01b3a7728a1f09e5c
alqemic <15896760+alqemic@users.noreply.github.com>
2024-05-19 22:49:30 +0200

    Use default kivy Settings

--- motiprompt/main.py
+++ motiprompt/main.py
@@ -1,5 +1,7 @@
 import kivy
+from kivy.logger import Logger
 from kivy.uix.screenmanager import FadeTransition, ScreenManager
+from kivy.uix.settings import SettingsWithTabbedPanel
 from kivymd.app import MDApp
 from screens import AddQuote, MyRoot, SettingsScreen, ShowQuotes
 
@@ -23,13 +25,48 @@ class MotiScreenManager(ScreenManager):
         self.add_widget(settings)
 
 
+class MySettingsWithTabbedPanel(SettingsWithTabbedPanel):
+    def on_close(self):
+        Logger.info("main.py: MySettingsWithTabbedPanel.on_close")
+
+    def on_config_change(self, config, section, key, value):
+        Logger.info("main.py: MySettingsWithTabbedPanel.on_config_change: " "{0}, {1}, {2}, {3}".format(config, section, key, value))
+
+
 class MotiPrompt(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Olive"
 
+        self.settings_cls = MySettingsWithTabbedPanel
+
         sm = MotiScreenManager()
         return sm
+
+    def build_config(self, config):
+        """
+        Set the default values for the configs sections.
+        """
+        config.setdefaults("My Settings", {"min_val": 0, "max_val": 10})
+
+    def build_settings(self, settings):
+        """
+        Add our custom section to the default configuration object.
+        """
+        settings.add_json_panel("My Settings", self.config, data="[]")
+
+    def on_config_change(self, config, section, key, value):
+        """
+        Respond to changes in the configuration.
+        """
+        Logger.info("main.py: App.on_config_change: {0}, {1}, {2}, {3}".format(config, section, key, value))
+
+    def close_settings(self, settings=None):
+        """
+        The settings panel has been closed.
+        """
+        Logger.info("main.py: App.close_settings: {0}".format(settings))
+        super(MotiPrompt, self).close_settings(settings)
 
 
 if __name__ == "__main__":
